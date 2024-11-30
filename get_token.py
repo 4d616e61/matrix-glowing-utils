d2e175d88e7ae8cfd853e849ba738bef6e52a7de
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-30 03:56:32 -0800

    idk

--- get_token.py
+++ get_token.py
@@ -1,9 +1,8 @@
 #!/bin/python
 import utils.matrix_utils as matrix_utils
 import sys
-import json
 
 
 matrix_utils.init_pg()
-res = matrix_utils.get_user_tokens(sys.argv[1])[0][0]
+res = matrix_utils.get_user_tokens(sys.argv[1])
 print(res)
