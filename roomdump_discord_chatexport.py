6153f855a6010a0439888648f175adff636d153c
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-12-04 22:26:08 -0800

    lmao

--- roomdump_discord_chatexport.py
+++ roomdump_discord_chatexport.py
@@ -6,7 +6,7 @@ matrix_utils.init_pg()
 
 
 
-res_pg = matrix_utils.get_all_events_matching(f"room_id='{sys.argv[1]}' limit 1", "type=m.room.message")
+res_pg = matrix_utils.get_all_events_matching(f"room_id='{sys.argv[1]}' limit 1", "type = 'm.room.message'")
 res = []
 for v in res_pg:
     v_parsed = json.loads(v[0])
