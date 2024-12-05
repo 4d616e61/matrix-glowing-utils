91bdf761ecdd347b4d61d8b579a049d9caf9f57b
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-12-04 22:29:17 -0800

    test

--- roomdump_discord_chatexport.py
+++ roomdump_discord_chatexport.py
@@ -4,9 +4,12 @@ import json
 
 matrix_utils.init_pg()
 
+def convert_entry(inp : dict) -> dict:
+    pass
 
 
-res_pg = matrix_utils.get_all_events_matching(f"room_id='{sys.argv[1]}' and type = 'm.room.message' limit 1")
+res_pg = matrix_utils.get_all_events_matching(f"room_id='{sys.argv[1]}' and type = 'm.room.message' limit 10")
+res_pg.sort(key= lambda d : d["origin_server_ts"])
 res = []
 for v in res_pg:
     v_parsed = json.loads(v[0])
