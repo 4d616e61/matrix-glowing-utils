dd3adc27ec1c3e1ee4ed84f671f1872cd180e4d2
Mana <57663038+4d616e61@users.noreply.github.com>
2025-01-01 02:56:03 -0800

    Update custom_events_matching.py

--- custom_events_matching.py
+++ custom_events_matching.py
@@ -5,13 +5,15 @@ import json
 matrix_utils.init_pg()
 
 
-
+#example: "sender='@tete:pettan.cc' and type='m.room.message'" "json like '%https://e_hentai.org%'"
 res = matrix_utils.get_all_events_matching(sys.argv[1], sys.argv[2] if len(sys.argv) > 2 else "")
 
 for v in res:
-    print("MESSAGE:")
-    print(json.loads(v[0])["content"]["body"])
-
+    #print("MESSAGE:")
+    try:
+        print(json.loads(v[0])["content"]["body"])
+    except:
+        pass
 
 
 
