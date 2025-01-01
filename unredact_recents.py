af55a2d16767f40abfba47d4e531b0c42c785d40
Mana <57663038+4d616e61@users.noreply.github.com>
2025-01-01 04:14:54 -0800

    Update unredact_recents.py

--- unredact_recents.py
+++ unredact_recents.py
@@ -2,6 +2,7 @@
 import utils.matrix_utils as matrix_utils
 import sys
 import json
+from utils.misc import *
 
 
 matrix_utils.init_pg()
@@ -14,12 +15,15 @@ if len(sys.argv) > 1:
 res_redact_events = matrix_utils.get_all_events_matching(f"type='m.room.redaction' order by origin_server_ts desc limit {limit}")
 events_final = []
 for ev in res_redact_events:
-    print(ev)
-    events_final.append(ev)
-print("RAW JSON:")
-print()
+    print(ev["redacts"])
+    events_final.append(matrix_utils.get_event(ev["redacts"])[0])
 
-print("\n\n\n")
-print("MESSAGE:")
-print(json.loads(res)["content"]["body"])
- 
+for ev in events_final:
+
+    print("RAW JSON:")
+    print(json_reserialize(ev))
+
+    print("\n\n\n")
+    print("MESSAGE:")
+    print(format_event_json(ev))
+    
