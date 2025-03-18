b0ef89085b547c8d8fb9b73fc76a57f6ba9c35d4
4d616e61 <57663038+4d616e61@users.noreply.github.com>
2025-03-18 00:19:27 -0700

    Update get_room_auth_events.py

--- get_room_auth_events.py
+++ get_room_auth_events.py
@@ -4,6 +4,6 @@ import sys
 import json
 
 matrix_utils.init_pg()
-res = matrix_utils.get_all_events_matching("where type='m.room.member' order by origin_server_ts desc limit 1")
+res = matrix_utils.get_all_events_matching(f"type='m.room.member' and room_id={sys.argv[1]} order by origin_server_ts desc limit 1")
 res = json.loads(res[0][0])
 print(res["auth_events"])
